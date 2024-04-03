41b4099eb4564f1f75de120bc09179c6dbe64249
caiioo0 <caio.rruda2019@gmail.com>
2024-04-03 13:50:35 -0200

    criando o formulario e adicionando os pedidos ao banco

---
+++ mudi/src/main/java/br/com/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,47 @@
+package br.com.mvc.mudi.dto;
+
+import br.com.mvc.mudi.Model.Pedido;
+
+public class RequisicaoNovoPedido {
+
+	private String nomeProduto;
+	private String urlImagem;
+	private String urlProduto;
+	private String descricaoPedido;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getDescricaoPedido() {
+		return descricaoPedido;
+	}
+	public void setDescricaoPedido(String descricaoPedido) {
+		this.descricaoPedido = descricaoPedido;
+	}
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(urlProduto);
+		pedido.setDescricaoPedido(descricaoPedido);
+		return pedido;
+	}
+	
+	
+	
+}
