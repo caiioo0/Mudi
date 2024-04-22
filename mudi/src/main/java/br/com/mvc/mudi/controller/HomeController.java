60c36225fc451ab31f7d0b9e3fb338665219dba2
caiioo0 <caio.rruda2019@gmail.com>
2024-04-22 12:51:33 -0300

    finalizando as rotas dos status dos pedidos

--- mudi/src/main/java/br/com/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -4,17 +4,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import br.com.mvc.mudi.Model.Pedido;
+import br.com.mvc.mudi.Model.StatusPedido;
 import br.com.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
 
 		List<Pedido> pedidos = pedidoRepository.findAll();
@@ -22,5 +28,19 @@ public class HomeController {
 
 		return "home";
 	}
+	
+	@GetMapping("/{status}")
+	public String porStatus(@PathVariable("status") String status, Model model) {
 
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+		model.addAttribute("pedidos", pedidos);
+		model.addAttribute("status", status);
+
+		return "home";
+	}	
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirec:/home";
+	}
 }
--- mudi/src/main/java/br/com/mvc/mudi/repository/PedidoRepository.java
+++ mudi/src/main/java/br/com/mvc/mudi/repository/PedidoRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.mvc.mudi.Model.Pedido;
+import br.com.mvc.mudi.Model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 	
 	public List<Pedido> findAll();
+
+	public List<Pedido> findByStatus(StatusPedido status);
 
 }
